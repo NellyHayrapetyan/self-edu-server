ef306d240247b769d9757d675951e60f696e9a20
nelly <nellylusine@gmail.com>
2020-08-29 23:22:41 +0400

    Change db to heroku postgreSql

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -2,11 +2,12 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: 'localhost',
+  host: 'ec2-54-158-122-162.compute-1.amazonaws.com',
   port: 5432,
-  username: 'postgres',
-  password: 'root',
-  database: 'self-edu',
+  username: 'ntvjimutfnlper',
+  ssl: true,
+  password: '765eda005b191864848558a6a649ffeeab4c278a418584de4ecd8519152553ca',
+  database: 'dadj7m1r1o7j94',
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
   synchronize: true,
 };
